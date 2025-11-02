c358829eb5661e6149119f62972fece9614f6b0c
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-02 18:41:21 +0530

    title and featured destination

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
+import Title from './Title'
+import { useNavigate } from 'react-router-dom'
 
 const FeaturedDestination = () => {
+  const navigate = useNavigate()
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+
+       <Title title='Feature Destination' subTitle='Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences. '/>
+
         <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
             {roomsDummyData.slice(0,4).map((room, index)=>(
                 <HotelCard key={room._id} room={room} index={index}/>
             ))}
         </div>
+        <button onClick={()=>{navigate('/rooms'); scrollTo(0,0)}} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
+          View All Destination
+        </button>
     </div>
   )
 }
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = ({title, subTitle, align, font}) => {
+    return (
+        <div className={`flex flex-col justify-center items-center text-center ${align === "left" && "md:items-start md:text-left"}`}>
+            <h1 className={`text-4xl md:text-[40px] ${font || "font-playfair"}`}>{title}</h1>
+            <p className="tect-sm md:text-base text-gray-500/90 mt-2 max-w-174">{subTitle}</p>
+        </div>
+    )
+}
+
+export default Title
