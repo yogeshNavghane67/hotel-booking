baea396642735b6901031f227525e3308802380a
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-03 23:51:57 +0530

    Testimonal section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonal from '../components/Testimonal'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero/>
         <FeaturedDestination/>
         <ExclusiveOffers/>
+        <Testimonal/>
     </>
   )
 }
