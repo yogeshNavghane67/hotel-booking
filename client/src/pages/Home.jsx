ef2e7b7ce481891dfa216da12b9cc66eaf7f4dfe
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-04 23:27:25 +0530

    newsletters component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonal from '../components/Testimonal'
+import NewsLetter from '../components/Newsletter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <FeaturedDestination/>
         <ExclusiveOffers/>
         <Testimonal/>
+        <NewsLetter/>
     </>
   )
 }
