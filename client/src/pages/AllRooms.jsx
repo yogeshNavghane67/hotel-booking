a75a6e403f5522e8b2a29b4226362a9e2dbfe571
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-05 23:47:32 +0530

    filters section

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,9 +1,50 @@
 import { useNavigate } from "react-router-dom"
 import { assets, facilityIcons, roomsDummyData } from "../assets/assets"
 import StarRating from "../components/StarRating";
+import { useState } from "react";
+
+const CheckBox = ({label, selected = false, onChange = () => { }})=> {
+    return (
+        <label className="flex gap-3 items-center cursor-pointer mt-2 text-sm">
+            <input type="checkbox" checked={selected} onChange={(e)=>onChange(e.target.checked, label)} />
+            <span className="font-light select-none">{label}</span>
+        </label>
+    )
+}
+
+const RedioButton = ({label, selected = false, onChange = () => { }})=> {
+    return (
+        <label className="flex gap-3 items-center cursor-pointer mt-2 text-sm">
+            <input type="radio" name="sortOption" checked={selected} onChange={(e)=>onChange(label)} />
+            <span className="font-light select-none">{label}</span>
+        </label>
+    )
+}
 
 const AllRooms = () => {
     const navigate = useNavigate();
+    const [openFilters, setOpenFilters] = useState(false)
+
+    const roomTypes = [
+        "Single Bed",
+        "Double Bed",
+        "Luxury Bed",
+        "Family Suite",
+    ];
+
+    const priceRanges = [
+        '0 to 500',
+        '500 to 1000',
+        '1000 to 2000',
+        '2000 to 3000',
+    ]
+
+    const sortOptions = [
+        "Price Low to High",
+        "Price High to Low",
+        "Newest First",
+    ]
+
   return (
     <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32">
         <div>
@@ -46,7 +87,38 @@ const AllRooms = () => {
 
         
         {/* filters */}
-        <div className="">
+        <div className="bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16">
+
+            <div className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
+                <p className="text-base font-medium text-gray-800">FILTERS</p>
+                <div className="text-xs cursor-pointer">
+                    <span onClick={()=> setOpenFilters(!openFilters)} className="lg:hidden">{openFilters ? 'HIDE' : 'SHOW'}</span>
+                    <span className="hidden lg:block">CLEAR</span>
+                </div>
+            </div>
+
+            <div className={`${openFilters ? 'h-auto' : "h-0 lg:h-auto"} overflow-hidden transition-all duration-700`}>
+                <div className="px-5 pt-5">
+                    <p className="font-medium text-gray-800 pb-2">Popular filters</p>
+                    {roomTypes.map((room, index)=>(
+                        <CheckBox key={index} label={room}/>
+                    ))}
+                </div>
+
+                <div className="px-5 pt-5">
+                    <p className="font-medium text-gray-800 pb-2">Price Range</p>
+                    {priceRanges.map((range, index)=>(
+                        <CheckBox key={index} label={`₹ ${range}`}/>
+                    ))}
+                </div>
+
+                <div className="px-5 pt-5 pb-7">
+                    <p className="font-medium text-gray-800 pb-2">Sort By</p>
+                    {sortOptions.map((option, index)=>(
+                        <RedioButton key={index} label={option}/>
+                    ))}
+                </div>
+            </div>
         </div>
     </div>
   )
