2092e63f11ff9616f7f1910772b5a5644b9b0063
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-06 22:36:12 +0530

    room checkout form box

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { assets, roomsDummyData } from '../assets/assets'
+import { assets, facilityIcons, roomsDummyData } from '../assets/assets'
 import StarRating from '../components/StarRating'
 
 const RoomDetails = () => {
@@ -47,6 +47,55 @@ const RoomDetails = () => {
             ))}
           </div>
         </div>
+
+        {/* Room Highlights  */}
+        <div className="flex flex-col md:flex-row md:justify-between mt-10">
+          <div className='flex flex-col'>
+            <h1 className='text-3xl md:text-4xl font-playfair'>Experience Luxury Like Never Before</h1>
+            <div className="flex flex-warp items-center mt-3 mb-6 gap-4">
+              {room.amenities.map((item, index)=> (
+                <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-gary-100'>
+                  <img src={facilityIcons[item]} alt={item} className='w-5 h-5' />
+                  <p className='text-xs'>{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Room Price  */}
+          <p className='text-2xl font-medium'>₹{room.pricePerNight}/night</p>
+        </div>
+
+        {/* CheckIn Checkout form  */}
+        <form className="flex flex-col md:flex-row items-start md:items-center justify-between bg-white shadow-[0px_0px_20px_rgba(0,0,0,0.15)] p-6 rounded-xl mx-auto mt-16 max-w-6xl">
+          
+          <div className="flex flex-col flex-wrap md:flex-row items-start md:items-center gap-4 md:gap-10 text-gray-500">
+            
+            <div className="flex flex-col">
+              <label htmlFor="checkInDate" className='font-medium'>Check-In</label>
+              <input type="date" id='checkInDate' placeholder='Check-In' className='w-full rounded border border-gary-300 px-3 py-2 mt-1.5 outline-none' required />
+            </div>
+
+          <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+           
+            <div className="flex flex-col">
+              <label htmlFor="checkOutDate" className='font-medium'>Check-Out</label>
+              <input type="date" id='checkOutDate' placeholder='Check-Out' className='w-full rounded border border-gary-300 px-3 py-2 mt-1.5 outline-none' required />
+            </div>
+         
+          <div className='w-px h-15 bg-gray-300/70 max-md:hidden'></div>
+         
+            <div className="flex flex-col">
+              <label htmlFor="guests" className='font-medium'>Guests</label>
+              <input type="number" id='guests' placeholder='0' className='max-w-20 rounded border border-gary-300 px-3 py-2 mt-1.5 outline-none' required />
+            </div>
+
+          </div>
+
+          <button type='submit' className='bg-primary hover:bg-primary-dull active:scale-95 transition-all text-white rounded-md max-md:w-full max-md:mt-6 md:px-25 py-3 md:py-4 text-base cursor-pointer'>
+            Check Availability
+          </button>
+        </form>
     </div>
   )
 }
