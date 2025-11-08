37a5a9daec74d5f8f481512df59e275cf8f84b7d
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-09 00:40:14 +0530

    HotelReg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,14 @@ import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar/>}
+      {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -59,10 +59,19 @@ const MyBookings = () => {
                         </div>
 
                         {/* Payment Status  */}
-                        <div>
-
+                        <div className="flex flex-col items-start justify-center pt-3">
+                            <div className="flex items-center gap-2">
+                                <div className={`h-3 w-3 rounded-full ${booking.isPaid ? "bg-green-500" : "bg-red-500"}`}></div>
+                                <p className={`text-sm ${booking.isPaid ? "text-green-500" : "text-red-500"}`}>
+                                    {booking.isPaid ? "Paid" : "Unpaid"}
+                                </p>
+                            </div>
+                            {!booking.isPaid && (
+                                <button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">
+                                    Pay Now
+                                </button>
+                            )}
                         </div>
-
                     </div>
                 ))}
             </div>
