57dbcd4a8b0960c3ade42b53558e1065f40f12fd
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-10 23:56:20 +0530

    dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
 import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -22,6 +25,9 @@ const App = () => {
           <Route path='/rooms/:id' element={<RoomDetails/>}/>
           <Route path='/my-bookings' element={<MyBookings/>}/>
           <Route path='/owner' element={<Layout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path='add-room' element={<AddRoom/>}/>
+            <Route path='list-room' element={<ListRoom/>}/>
           </Route>
         </Routes>
       </div>
--- client/src/pages/hotelOwner/Deshboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Deshboard = () => {
-  return (
-    <div>Deshboard</div>
-  )
-}
-
-export default Deshboard
