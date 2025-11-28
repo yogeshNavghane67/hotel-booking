6e90e9f851a0469d4451f4e7c2e8e556d6635848
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-11-29 00:18:11 +0530

    add hotel by user

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const hotelSchema = new mongoose.Schema({
     name: { type: String, required: true },
     address: { type : String, required: true },
-    constact: { type: String, required: true },
+    contact: { type: String, required: true },
     owner: { type: String, required: true, ref: "user" },
     city: { type: String, required: true },
 },{timestamps: true});
